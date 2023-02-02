b6f9ec227f89f36ca387d79ab121a08f1ea80846
venjerin <vldvrb@bk.ru>
2023-02-02 20:35:59 +0300

    lesson 45_mapStateToProps_mapDispatchToProps_buttons_isnt_working

--- my-app/src/Components/Dialogs/DialogsContainer.jsx
+++ my-app/src/Components/Dialogs/DialogsContainer.jsx
@@ -1,26 +1,48 @@
 import React from "react";
+import { connect } from "react-redux";
 import { addMessageActionCreator, updateNewMessageActionCreator } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
 
 
-const DialogsContainer = (props) => {
-    let dialogs = props.dialogs;
-    let messages = props.messages;
+// const DialogsContainer = (props) => {
+//     let dialogs = props.dialogs;
+//     let messages = props.messages;
 
-    let sendMessage = () => {
-        props.dispatch(addMessageActionCreator())
+//     let sendMessage = () => {
+//         props.dispatch(addMessageActionCreator())
+//     }
+
+//     let messageOnChange = (text) => {
+//         // let text = newMessageElement.current.value;
+//         props.dispatch(updateNewMessageActionCreator(text));
+//     }
+//     return <Dialogs updateNewMessageBody={messageOnChange}
+//         sendMessage={sendMessage}
+//         dialogs={dialogs}
+//         messages={messages}
+//         newMessageText={props.newMessageText}></Dialogs>
+// };
+
+
+let mapStateToProps = (state) => {
+    return {
+        dialogs: state.profilePage.dialogs,
+        messages: state.dialogsPage.messages,
+        // newMessageText: state.dialogsPage.newMessageText,
     }
+}
 
-    let messageOnChange = (text) => {
-        // let text = newMessageElement.current.value;
-        props.dispatch(updateNewMessageActionCreator(text));
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (text) => {
+            dispatch(updateNewMessageActionCreator(text));
+        },
+        sendMessage: () => {
+            dispatch(addMessageActionCreator())
+        }
     }
+}
 
-    return <Dialogs updateNewMessageBody = {messageOnChange}
-    sendMessage = {sendMessage}
-    dialogs = {dialogs}
-    messages = {messages}
-    newMessageText = {props.newMessageText}></Dialogs>
-};
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
